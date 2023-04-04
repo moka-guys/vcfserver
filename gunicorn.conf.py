23f8b39022974f26c63a74d3b8592e698d80cc1f
David Brawand <dbrawand@gmail.com>
2023-04-04 10:16:49 +0100

    updated gunicorn settings to prevent worker timeouts

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -13,3 +13,5 @@ workers = 1  # multiprocessing.cpu_count() * 2 + 1
 timeout = 30  # timeout 30 seconds
 keepalive = 60 * 60  # keep connections alive for 1 hour
 capture_output = True
+worker_tmp_dir = '/dev/shm'  # use /dev/shm to prevent worker timeouts
+worker_class = 'gthread'  # use gthread to prevent worker timeouts
